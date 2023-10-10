ba6d13116df1f74cb49257f68ed88f4559a4eae2
machado-joao <joaolucas.cm@hotmail.com>
2023-10-10 13:10:55 -0300

    feat: Configure database and implement user creation

--- src/main/java/com/github/todolist/user/controller/UserController.java
+++ src/main/java/com/github/todolist/user/controller/UserController.java
@@ -1,18 +1,32 @@
 package com.github.todolist.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.todolist.user.models.User;
+import com.github.todolist.user.repositories.IUserRepository;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/create")
-    public String create(@RequestBody User user) {
-        return "User created.";
+    public ResponseEntity<?> create(@RequestBody User newUser) {
+
+        var user = this.userRepository.findByUsername(newUser.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already taken.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(newUser));
     }
 }
--- src/main/java/com/github/todolist/user/models/User.java
+++ src/main/java/com/github/todolist/user/models/User.java
@@ -1,13 +1,33 @@
 package com.github.todolist.user.models;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String name;
+
+    @Column(unique = true)
     private String username;
+
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
